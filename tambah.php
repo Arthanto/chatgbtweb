1c868b6666f784f9f57674cd652b3373c0b9e6df
Restu <rtutu1749@gmail.com>
2025-12-04 10:14:47 +0700

    Create tambah.php for photo upload form

---
+++ tambah.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head><title>Tambah Foto</title></head>
+<body>
+<h2>Tambah Foto</h2>
+<form action="proses_tambah.php" method="post" enctype="multipart/form-data">
+Pilih Foto:<br>
+<input type="file" name="foto" required><br><br>
+<button type="submit">Upload</button>
+</form>
+</body>
+</html>
